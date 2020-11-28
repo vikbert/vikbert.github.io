5f808694e2bc0ffc34aafe517711c4231d8aa1c7
Xun Zhou <segentor@gmail.com>
2020-11-28 19:09:07 +0100

    updates

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,19 @@
-mkdir -p gh-pages
-cp -rf src/* gh-pages
-cd gh-pages
+#!/usr/bin/env sh
+set -e
 
+readonly GIT_REPO=https://github.com/vikbert/vikbert.github.io.git
+
+npm run export
+mkdir -p gh-pages
+cp -rf __sapper__/export/* gh-pages/
+
+cd gh-pages
 git init
 git add -A
 
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
 git commit -m "deploy: github gh-pages $DATE"
-
-git push -f https://github.com/vikbert/vikbert.github.io.git master:gh-pages
+git push -f $GIT_REPO master:gh-pages
 
 cd -
 git pull
