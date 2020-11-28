801324c12f425a9811e74caeb2ac3afbd242d4a8
Xun Zhou <segentor@gmail.com>
2020-11-28 19:23:12 +0100

    updates

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,14 @@ npm run export
 mkdir -p gh-pages
 cp -rf __sapper__/export/* gh-pages/
 
-cd gh-pages
-git init
-git add -A
+# cd gh-pages
+# git init
+# git add -A
 
-DATE=$(date '+%Y-%m-%d %H:%M:%S')
-git commit -m "deploy: github gh-pages $DATE"
-git push -f $GIT_REPO master:gh-pages
+# DATE=$(date '+%Y-%m-%d %H:%M:%S')
+# git commit -m "deploy: github gh-pages $DATE"
+# git push -f $GIT_REPO master:gh-pages
 
-cd -
-git pull
-rm -rf gh-pages
+# cd -
+# git pull
+# rm -rf gh-pages
