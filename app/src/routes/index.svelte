edb73eae62486fca7d6801e0b3a2d4bee9ccd599
Xun Zhou <segentor@gmail.com>
2020-11-30 21:31:05 +0100

    deploy: 2020-11-30 21:31:05

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -31,21 +31,21 @@
 
   <p>
     Hi, I'm
-    <strong>Vikbert</strong>. This cybername comes from the names of my two
+    <strong>Vikbert</strong> from Berlin. This cybername comes from the names of my two
     kids:
     <strong>Vik</strong>toria & Al<strong>bert</strong>.
   </p>
   <p>
-    I work as a software engineer in Berlin and enjoy my fullstack job at
-    Schwarz Gruppe. Mostly I use the the Symfony framework
+    I work as a software engineer since 2010, after my "Diplom Informatik"
+    study at TU Berlin. Mostly, I use the the Symfony framework
     <a
       target="_blank"
       href="https://connect.symfony.com/profile/vikbert"
       rel="noreferrer">
-      <strong>(connect me)</strong>
+      <strong>(sf connect)</strong>
     </a>
-    for the web projects at work and love the symfony components and its
-    community. I am interested in "Domain Driven Design" and become an absolute
+    for the web projects and love its decoupled and resuable components.
+    I am interested also in "Domain Driven Design" and become an absolute
     believer of both
     <strong>Clean code</strong>
     and
@@ -62,13 +62,19 @@
       href="https://www.exin.com/"
       target="_blank"
       rel="noreferrer"><strong>EXIN</strong></a>
-    certificated Scrum Master and Product Owner and live in Agile software
-    development at daily work.
+    certificated Scrum Master and Product Owner and live actively in Agile
+    Software Development for daily work. In my experience, agile sofware
+    development is about "People first, transparency and constantly improving".
+    For that vision, I follow the
+    <a
+      href="https://agilemanifesto.org/"
+      target="_blank"
+      rel="noreferer noopener"><strong>Manifesto for Agile Software Development</strong></a>.
   </p>
 
   <p>
-    In my spare time, I just jump out of the world of Object-orientation and do
-    some lightweight coding in open-source world with both
+    In my spare time, I just jump out of the world of <strong>server-side programming</strong> and do
+    some reactive & functional programming in open-source world with both
     <strong>React</strong>
     and
     <strong>Svelte</strong>.
@@ -82,13 +88,22 @@
 
   <div class="stack">
     <div class="my-1">Experimental Builds With</div>
-    <a href="https://vikbert.github.io/pixss/" rel="noreferer noopener" target="_blank">
-      <img src="pixss.png" alt="pixss" width="32">
+    <a
+      href="https://vikbert.github.io/pixss/"
+      rel="noreferer noopener"
+      target="_blank">
+      <img src="pixss.png" alt="pixss" width="32" />
     </a>
     <a href="https://svelte.dev/" rel="noreferer noopener" target="_blank">
-      <span class="iconify" data-icon="teenyicons:svelte-outline" data-inline="false" />
+      <span
+        class="iconify"
+        data-icon="teenyicons:svelte-outline"
+        data-inline="false" />
     </a>
-    <a href="https://www.markdownguide.org/" rel="noreferer noopener" target="_blank">
+    <a
+      href="https://www.markdownguide.org/"
+      rel="noreferer noopener"
+      target="_blank">
       <span class="iconify" data-icon="bi:markdown" data-inline="false" />
     </a>
   </div>
