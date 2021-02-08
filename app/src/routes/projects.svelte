a9670d7204842d07637ad5305320ff2601a2e6f8
Xun Zhou <segentor@gmail.com>
2021-02-08 10:48:14 +0100

    updates

--- app/src/routes/projects.svelte
+++ app/src/routes/projects.svelte
@@ -31,6 +31,7 @@
         'Another Chrome extension to increase the productivity with the following features: manage Todos, quick search your bookmarks, add add a text note from everywhere.',
       demo: 'https://github.com/vikbert/nutab',
       repository: 'vikbert/nutab',
+      tags: ['sass', 'react', 'vue'],
       icon:
         '<span class="iconify" data-icon="ant-design:chrome-outlined" data-inline="false"></span>',
     },
@@ -40,6 +41,7 @@
         'A simple mobile app icon generator to save up the time to create the app icon for both native app or web app',
       demo: 'https://github.com/vikbert/appikon',
       repository: 'vikbert/appikon',
+      tags: ['sass', 'react', 'vue'],
       icon:
         '<span class="iconify" data-icon="ri:reactjs-fill" data-inline="false"></span>',
     },
@@ -49,6 +51,7 @@
         'A Chrome extension to sve up to 100% memory and reduce tab clutter. Close or restore these tabs with just one click.',
       demo: 'https://github.com/vikbert/yitab',
       repository: 'vikbert/yitab',
+      tags: ['sass', 'react', 'vue'],
       icon:
         '<span class="iconify" data-icon="ant-design:chrome-outlined" data-inline="false"></span>',
     },
@@ -58,6 +61,7 @@
         'A couple of shell scripts for setting up the web dev environment in MacOS',
       demo: 'https://github.com/vikbert/awesome-macbook-setup',
       repository: 'vikbert/awesome-macbook-setup',
+      tags: ['macos', 'cli'],
       icon:
         '<span class="iconify" data-icon="carbon:mac-command" data-inline="false"></span>',
     },
@@ -66,6 +70,7 @@
       info: 'My mini technical notes, build with vue.js and markdown.',
       demo: 'https://vikbert.github.io/nines/',
       repository: 'vikbert/nines',
+      tags: ['vue'],
       icon:
         '<span class="iconify" data-icon="logos:markdown" data-inline="false"></span>',
     },
@@ -75,6 +80,7 @@
         'A realtime mask radar built with Vue.js for Google Map. The test data is found in internet, and is not real.',
       demo: 'https://github.com/vikbert/maskr/',
       repository: 'vikbert/maskr',
+      tags: ['sass', 'vue'],
       icon:
         '<span class="iconify" data-icon="ri:vuejs-line" data-inline="false"></span>',
     },
@@ -84,6 +90,7 @@
         'BookStore: a experimental app built with Svelte 3 and mocked REST API as I started learning svelte.js',
       demo: 'https://github.com/vikbert/svelete-bookstore',
       repository: 'vikbert/svelete-bookstore',
+      tags: ['sass', 'svelte',],
       icon:
         '<span class="iconify" data-icon="teenyicons:svelte-outline" data-inline="false"></span>',
     },
@@ -93,6 +100,7 @@
         'Test the internet connection speed and ping speedtest.net from the CLI',
       demo: 'https://github.com/vikbert/speedor',
       repository: 'vikbert/speedor',
+      tags: ['cli', 'react'],
       icon:
         '<span class="iconify" data-icon="mdi:speedometer-medium" data-inline="false"></span>',
     },
@@ -102,6 +110,7 @@
         'A smart full list of Symfony components for simple search and category.',
       demo: 'https://github.com/vikbert/sf-components',
       repository: 'vikbert/sf-components',
+      tags: ['sass', 'svelte',],
       icon:
         '<span class="iconify" data-icon="teenyicons:svelte-outline" data-inline="false"></span>',
     },
@@ -110,6 +119,7 @@
       info: 'A minimal fitness timer made for interval training.',
       demo: 'https://github.com/vikbert/masanfit',
       repository: 'vikbert/masanfit',
+      tags: ['sass', 'react', ],
       icon:
         '<span class="iconify" data-icon="ion:fitness-outline" data-inline="false"></span>',
     },
