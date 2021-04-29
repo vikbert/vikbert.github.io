db23cdf0e992fc38dc80070448a84bfb475dccd8
vikbert <segentor@gmail.com>
2021-04-29 10:03:17 +0200

    updates

--- app/src/routes/projects.svelte
+++ app/src/routes/projects.svelte
@@ -9,6 +9,7 @@
 
 <HeroShape color="is-secondary text-centered">
     <h2 class="opacity-50">Some of my side projects</h2>
+    <h5 class="opacity-75">Most of my PHP side projects are related to working stuffs, and they are not shared on GitHub. </h5>
 </HeroShape>
 
 <ProjectList/>>
