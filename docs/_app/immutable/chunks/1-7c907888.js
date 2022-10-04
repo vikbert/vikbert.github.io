f7258821fdefa2653df2143f16c55a2a29490624
vikbert <segentor@gmail.com>
2022-10-04 22:54:15 +0200

    updates

--- docs/_app/immutable/chunks/1-7c907888.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-3c324832.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
             fallback: null,
         }),
         paths: {
-            base: dev ? '' : '/vikbert.github.io',
+            base: dev ? '' : '',
         },
     },
 };
